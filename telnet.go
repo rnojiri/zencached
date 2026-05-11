cfb14ca0a54067dc58091ab58203e5165ff9c535
Rafael Nojiri <rafael.nojiri@gympass.com>
2026-05-11 17:24:39 -0300

    Trying a fix.

--- telnet.go
+++ telnet.go
@@ -139,7 +139,7 @@ func (t *Telnet) checkConnection() {
 				if logh.ErrorEnabled {
 					t.logger.Error().Msgf("error resolving telnet connection address: %s", t.node.String())
 				}
-				continue
+				return
 			}
 
 			if !t.address.IP.Equal(address.IP) {
@@ -147,7 +147,7 @@ func (t *Telnet) checkConnection() {
 				if logh.ErrorEnabled {
 					t.logger.Error().Msgf("telnet connection ip changed: %s -> %s", t.address.IP.String(), address.IP.String())
 				}
-				continue
+				return
 			}
 
 			connection, err := net.DialTCP("tcp", nil, address)
@@ -156,7 +156,7 @@ func (t *Telnet) checkConnection() {
 				if logh.ErrorEnabled {
 					t.logger.Error().Msgf("error testing telnet connection to ip address: %s", address.IP.String())
 				}
-				continue
+				return
 			}
 
 			err = connection.Close()
@@ -524,8 +524,8 @@ func (t *Telnet) reportDisconnection() {
 		return
 	}
 
-	t.onFailure.CompareAndSwap(false, true)
 	t.disconnectionChannel <- struct{}{}
+	t.onFailure.CompareAndSwap(false, true)
 
 	if logh.InfoEnabled {
 		t.logger.Info().Msg("connection failure reported")
